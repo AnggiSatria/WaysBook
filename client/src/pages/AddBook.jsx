00ad74915b76e897001fa6f90600cddb9b294f2a
AnggiSatria <anggisatria122@gmail.com>
2022-07-09 17:39:10 +0700

    updated

--- client/src/pages/AddBook.jsx
+++ client/src/pages/AddBook.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
+import Form from '../components/add book/form'
+import AdminNavbar from '../components/navbar/AdminNavbar'
 
 function AddBook() {
   return (
-    <div>add book</div>
+    <div>
+      <div className="navbar">
+        <AdminNavbar/>
+      </div>
+
+      <div className="form" style={{marginLeft : "10%", marginRight : "10%", marginTop : "50px"}}>
+        <Form/>
+      </div>
+    </div>
   )
 }
 
